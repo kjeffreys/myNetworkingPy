d16f4e3c9a1b9acab2bae49e29bb96c27a841dfe
kjeffreys <kajeffr1@asu.edu>
2020-02-27 14:28:52 -0500

    More mouse automation. Organized directories

---
+++ pythonAutoGui/mouseApi/mouseRelativePositionMvmt.py
@@ -0,0 +1,6 @@
+import pyautogui
+for i in range(10):
+    pyautogui.move(100, 0, duration=0.25)   # right
+    pyautogui.move(0, 100, duration=0.25)   # down
+    pyautogui.move(-100, 0, duration=0.25)  # left
+    pyautogui.move(0, -100, duration=0.25)  # up
