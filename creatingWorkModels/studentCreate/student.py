f3a25e18d4ec422b0192f45b5e832b43d454fd56
kjeffreys <kajeffr1@asu.edu>
2020-03-25 13:13:57 -0400

    Successfully created one way of templating student accounts for names, username, pw

--- creatingWorkModels/studentCreate/student.py
+++ creatingWorkModels/studentCreate/student.py
@@ -1,73 +1,98 @@
-"""
-@see https://docs.python.org/3/tutorial/classes.html
-"""
-def makeNewStudent():
-    g = eval(input("Enter student\n"))
-    el = g.split()
-    print(g)
-    print(el)
+import time
 
-'''
 class Student:
-    # username = ""  # stud=fInit+lInit+lunch
-    # pw = ""
-    context = ""
-    template = ""
-    volume = ""
-    homeDir = ""
-    first = 'hi'
-    """
-    A student obj with fName, lName, lunch, grade)
-    """
+    category = "Student"
+    schools = ["CHR, ELD, FLE, MAR"]
+
+    def __init__(self):
+        print("Student created")
+        self.fname = " "
+        self.lname = " "
+        self.lunch = " "
+        self.pw = " "
+        self.username = " "
+
+    def setNameLunch(self, info):
+        self.info = info.split()
+        print('1: {} 2: {} 3: {}'.format(self.info[0], self.info[1], self.info[2] ) )
+        self.fname = self.info[0]
+        self.lname = self.info[1]
+        self.lunch = self.info[2]
+        self.pw = self.info[2]
+        #working so far
+    
+    def setByPrompt(self):
+        print("Enter name length (2,3)")
+        data = input()
+        components = data.split()
+        for i in range(len(components)):
+            print("Element {}: {}".format(i, components[i]))
+
+    #short prompt to save typing
+    def prm(self):
+        print('Enter info:')
+        data1 = input()
+        components = data1.split()
+        print(components)
+
+        print("Name only or name and id? ([1 for NO], [2 for name AND id]")
+        # NOTE: Had issue where if not using int, the conditionals will not execute
+        # Python reads the input as a string. Does not convert to a number unless told to do so for logical evaluations
+        data2 = int(input())
+
+        print("For name...Enter 2 if len=2(fname, lname), Enter 3 if len=3(fname, paternal, maternal)")
+        nameLen = int(input())
+
+
+
+        #lunch id not provided
+        if(data2 == 1):
+            self.fname = components[0]
+            if(nameLen == 2):
+                self.lname = components[1]
+                print("I'm in data2==1 and nameLen==2")
+            elif(nameLen == 3):
+                paternal = components[1]
+                maternal = components[2]
+                self.lname = " ".join([paternal, maternal])
+                #working up to here
+                
+                #print("I'm in data2==1 and nameLen==3")
+        #lunch id provided
+        elif(data2 == 2):
+            self.fname = components[0]
+            if(nameLen == 2):
+                self.lname = components[1]
+                self.lunch = components[2]
+                self.pw = components[2]
+                self.username = "".join([components[0][0],components[1][0],components[2]])
+                print("I'm in data2==2 and nameLen==2")
+            if(nameLen == 3):
+                self.fname = components[0]
+                self.lname = " ".join([components[1], components[2]])
+                self.lunch = components[3]
+                self.pw = components[3]
+                self.username = "".join([components[0][0],components[1][0],components[3]])
+                #print("I'm in data2=2 and nameLen==3")
+
+        print("Successful execution of prm (prompt setting of student)")
+
+    def myVals(self):
+        print("Username: {}".format(self.username))
+        print("First name: {}".format(self.fname))
+        print("Last name: {}".format(self.lname))
+        print("Password: {}".format(self.lunch))
+        '''
+        >>> my.myVals()
+        Username: HD889521
+        First name: Henry
+        Last name: Duarte Perez
+        Password: 889521
+        '''
+
 '''
-'''
-    def __init__(self, 
-        self.fName = args[0]
-        self.lName = args[1]
-        self.lunch = args[2]
-        self.username = args[0][0] + args[1][0] + args[2][0]
-        self.pw = args[2]
-
-    def setGrade(self, grade):
-        gradeMap = {"K": "32", "1": "31", "2": "30",
-                    "3": "29", "4": "28", "5": "27", "6": "26", "7": "25",
-                    "8": "24"
-                    }
-        self.grade = grade
-
-        # K~32 1~31 2~30 3~29 4~28 5~27 6~26 7~25 8~26
-        self.adjustedGrade = gradeMap[grade]
-
-        # Context (5th)grade = 27.Intermediate.Students.eUsers.ERCSD
-        contextVars = [self.adjustedGrade,
-                       "Intermediate", "Students", "eUsers.ERCSD"]
-        self.context = ".".join(contextVars)
-
-        # Template:(5th) user_template.27.Intermediate.Students.eUsers.ERCSD
-        self.template = ".".join(
-            "user_template", self.adjustedGrade, "Intermediate", "Students", "eUsers.ERCSD")
-
-        # Volume: (5th)50-int-fs_VOL1.Servers.ERCSD
-        self.volume = ("50-int-fs_VOL1" + ".Servers.ERCSD")
-
-        # Path: \HOMEDIRS\STUDENTS\2027\
-        self.homeDir = "\\".join("HOMEDIRS", "STUDENTS", "2027")
-
-    def prSt(self):
-        print(self.username)
-        print(self.fname)
-        print(self.lname)
-        print(self.context)
-        print(self.template)
-        print(self.volume)
-        print(self.homeDir)
-
-
-"""
-Success!:
->>> kid1 = Student('ky','je', 1, 4)
->>> kid1
-<__main__.Student object at 0x00000246D012EE50>
-"""
-# could extend to inherit with schools
+Note:
+Remember to create an object of the class Student,
+using module student. So:
+newStudent = student.Student('str1', 4)
 '''
