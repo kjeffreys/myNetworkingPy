572d64a210f8f682b97de305668b514730a152b1
kjeffreys <kajeffr1@asu.edu>
2020-04-03 19:36:11 -0400

    Created parent user class for users

---
+++ classesForDb/eUsers.py
@@ -0,0 +1,54 @@
+class Euser:    #General superclass
+    '''
+    Attributes go first
+    '''
+
+    '''
+    Methods below
+    '''
+    
+    def createUser(self):
+        print("createUser()")
+    
+    def deleteUser(self):
+        print("deleteUser()")
+    
+    def disableAcct(self):
+        print("disableAcct()")
+
+    def enableAcct(self):
+        print("enableAcct()")
+
+    def modifyUser(self):
+        print("modifyUser()")
+
+    def moveUser(self):
+        print("moveUser()")
+    
+    def renameUser(self):
+        print("renameUser()")
+
+    def setUsername(self):
+        print("setUsername()")
+
+    def setFirstName(self):
+        print("setFirstName()")
+
+    def setLastName(self):
+        print("setLastName()")
+
+    def setContext(self):
+        print("setContext()")
+
+    def setTemplate(self):
+        print("setTemplate")
+        # Note: some are "User_template". Others are "user_template"
+        # Need to map accurately for all or do manually with pyautogui
+        # Need to map grade attr also if student.
+        # Need to map several other permission templates for admins
+
+    def setPassword(self):
+        print("setPassword()")
+
+    def setHomeDir(self):
+        print("setHomeDir()")
